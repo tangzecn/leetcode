8984708620b1f8827a33b19c65899d79e26722d5
tangzecn <tangzecn@gmail.com>
2015-06-18 21:28:13 -0700

    update contains duplicate

--- c++/217-ContainsDuplicate-2.cpp
+++ c++/217-ContainsDuplicate-2.cpp
@@ -6,12 +6,8 @@ using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> hashSet;
-        for (int i=0; i<nums.size(); i++) {
-            if (hashSet.find(nums[i]) != hashSet.end()) return true;
-            hashSet.insert(nums[i]);
-        }
-        return false;
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        return numSet.size() != nums.size();
     }
 };
 
