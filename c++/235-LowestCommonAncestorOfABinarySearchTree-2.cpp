e700914b399f0327b02863ff3ff3816e828d09a7
tangzecn <tangzecn@gmail.com>
2016-01-12 22:03:08 -0800

    update 235

--- c++/235-LowestCommonAncestorOfABinarySearchTree-2.cpp
+++ c++/235-LowestCommonAncestorOfABinarySearchTree-2.cpp
@@ -14,16 +14,17 @@ public:
     // If there can be same values in the tree.
     // The time complexity is O(N)
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (p == NULL || q == NULL) return NULL;
-        
-        while (root != NULL && root != p && root != q && !(root->val < p->val xor root->val < q->val)) {
-            if (root->val < p->val && root->val < q->val) {
-                root = root->right;
-            } else {
-                root = root->left;
-            }
-        }
-        return root;
+       if (root == nullptr || root == p || root == q) return root;
+       TreeNode *left = nullptr;
+       if (root->val >= p->val || root->val >= q->val) {
+           left = lowestCommonAncestor(root->left, p, q);
+       }
+       TreeNode *right = nullptr;
+       if (root->val <= p->val || root->val <= q->val) {
+           right = lowestCommonAncestor(root->right, p, q);
+       }
+       if (left != nullptr && right != nullptr) return root;
+       return (left != nullptr) ? left : right;
     }
 };
 
