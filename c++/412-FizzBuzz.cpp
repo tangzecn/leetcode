f7312599707cbe0e6919b16d2be636d80c537ea7
tangzecn <tangzecn@gmail.com>
2016-10-16 19:58:01 -0700

    add 412

---
+++ c++/412-FizzBuzz.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for (int i=1; i<=n; i++) {
+            if (i % 15 == 0) {
+                res.push_back("FizzBuzz");
+            } else
+            if (i % 3 == 0) {
+                res.push_back("Fizz");
+            } else 
+            if (i % 5 == 0) {
+                res.push_back("Buzz");
+            } else {
+                res.push_back(std::to_string(i));
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    return 0;
+}
